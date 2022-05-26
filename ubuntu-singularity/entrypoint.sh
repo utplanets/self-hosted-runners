7be8c064499d636c5d74773f420b32cf1e722aa9
Christopher Lee <lee@foldmountain.com>
2022-05-25 22:12:28 -0400

    updating ubuntu singularity

--- ubuntu-singularity/entrypoint.sh
+++ ubuntu-singularity/entrypoint.sh
@@ -2,10 +2,11 @@
 
 export RUNNER_USERNAME=$(id -un)
 export RUNNER_USERGROUP=$(id -gn)
-
+export RUNNER_WORKDIR="/tmp/actions-runner-repo"
 export AGENT_TOOLSDIRECTORY=/opt/hostedtoolcache
 
 cd /opt/actions-runner
+source .env
 
 if [[ -z "${RUNNER_NAME}" ]]; then
     RUNNER_NAME="singularity-$(hostname)"
@@ -18,6 +19,15 @@ PAYLOAD=$(curl -sX POST -H "Authorization: token ${PERSONAL_ACCESS_TOKEN}" ${ACT
 export RUNNER_TOKEN=$(echo $PAYLOAD | jq .token --raw-output)
 
 printf "\n\033[0;44m---> Configuring the runner.\033[0m\n"
+echo ./config.sh \
+    --name ${RUNNER_NAME} \
+    --token ${RUNNER_TOKEN} \
+    --url https://github.com/${GITHUB_ORG}/${GITHUB_REPO} \
+    --work ${RUNNER_WORKDIR} \
+    --labels "singularity,github" \
+    --unattended \
+    --replace
+
 ./config.sh \
     --name ${RUNNER_NAME} \
     --token ${RUNNER_TOKEN} \
